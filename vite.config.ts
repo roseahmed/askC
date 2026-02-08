1a837acda98cb846774a1e34f11868567ff80192
Rose Ahmed <roseahmed2013@gmail.com>
2026-02-09 03:46:47 +0530

    vite config added

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
-import { fileURLToPath } from "url";
 
-// Convert import.meta.url to __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-      "@components": path.resolve(__dirname, "src/components"),
-    },
+  base: "./", // Ensures relative pathing
+  build: {
+    assetsDir: "static", // Changing 'assets' to 'static' avoids GitHub's folder filters
+    outDir: "dist",
   },
 });
